744e747f20b88455c92fc72e3ed95b99ecd17bf4
Huidae Cho <grass4u@gmail.com>
2019-09-18 13:18:37 -0400

    Initial commit

---
+++ test_mysum.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+################################################################################
+# Name:    test_mysum.py and mysum.py
+# Purpose: These scripts demonstrate how to communicate with another Python
+#          script bi-directionally.
+# Usage:   ./test_mysum.py mysum.py
+# Author:  Huidae Cho
+# Since:   September 18, 2019
+#
+# Copyright (C) 2019, Huidae Cho <https://idea.isnew.info>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+################################################################################
+
+import sys
+import subprocess
+import os
+from random import randint
+
+def mysum(n):
+    x = 0
+    for i in range(1, n+1):
+        x += i
+    return x
+
+p = subprocess.Popen(['python3', sys.argv[1]],
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+while True:
+    line = os.read(p.stdout.fileno(), 256).decode()
+    if line == '':
+        break
+    elif line == 'n? ':
+        n = randint(1, 100)
+        inp = '{n}'.format(n=n) + os.linesep
+        p.stdin.write(inp.encode())
+        p.stdin.flush()
+    else:
+        ans = int(line)
+        if ans == mysum(n):
+            print('OK')
+        else:
+            print('What?')
